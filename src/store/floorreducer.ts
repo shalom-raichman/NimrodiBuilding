dbec1604de3f05ebce2fe61c2095ff0d48c5c236
Shalom <shlomo000@gmail.com>
2024-11-07 10:36:20 +0200

    configre floorReducer

--- src/store/floorreducer.ts
+++ src/store/floorreducer.ts
@@ -1,1 +1,41 @@
 //FILL HERE 3.1
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface  FloorState{
+  floor1: boolean
+  floor2: boolean
+  floor3: boolean
+  floor4: boolean
+  floor5: boolean
+}
+
+const initialState: FloorState = {
+  floor1: false,
+  floor2: false,
+  floor3: false,
+  floor4: false,
+  floor5: false,
+}
+
+type floors =
+  'floor1' |
+  'floor2' |
+  'floor3' |
+  'floor4' |
+  'floor5'
+
+export const floorReducer = createSlice({
+  name: 'floorAccess',
+  initialState,
+  reducers: {
+    changeAccess : (state: FloorState, action: PayloadAction<number>) => {
+
+      state[(`floor${action.payload}`as floors)] = !state[(`floor${action.payload}`as floors)]
+    },
+  },
+})
+
+export const {changeAccess} = floorReducer.actions
+
+export default floorReducer.reducer
