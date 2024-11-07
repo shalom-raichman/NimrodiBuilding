1d76e2fbf59262936f41ee18106645f2ff0a3714
Shalom <shlomo000@gmail.com>
2024-11-07 11:19:19 +0200

    role rducer added

--- src/store/rolereducer.ts
+++ src/store/rolereducer.ts
@@ -1,1 +1,25 @@
 //FILL HERE 3.2
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import roles from '../data/roles.json'
+
+export interface RoleState {
+  role: RoleType
+}
+
+type RoleType = "Unknown Personnel" | "Guest" | "Student" | "Developer" | "Executive Personnel"
+
+const initialState: RoleState = {role: roles[0] as RoleType}
+
+export const RoleReducer = createSlice({
+  name: 'roleAccess',
+  initialState,
+  reducers: {
+    setRole : (state: RoleState, action: PayloadAction<number>) => {
+      state.role = roles[action.payload] as RoleType
+    },
+  },
+})
+
+export const {setRole} = RoleReducer.actions
+
+export default RoleReducer.reducer
