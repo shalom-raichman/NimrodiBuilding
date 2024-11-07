0321164db016d284046d7ade316685b1963c1489
Shalom <shlomo000@gmail.com>
2024-11-07 10:37:04 +0200

    configur stor and embeded floor reducer to the stor

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,14 @@
 // FILL HERE 3.3
+
+import { configureStore } from '@reduxjs/toolkit'
+import  floorReducer  from './floorreducer'
+
+export const store = configureStore({
+  reducer: {
+    floor: floorReducer
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
